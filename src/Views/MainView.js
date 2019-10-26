82e8e5bfda10fcf498adc923125156ed6a1e250b
Kamil <kameq55@gmail.com>
2019-10-26 10:46:05 +0200

    Fetching data from server and displaying in main view as Id's of top stories

--- src/Views/MainView.js
+++ src/Views/MainView.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 function MainView() {
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const [stories, setStories] = useState([]);
+
+  const dataUrl = 'https://hacker-news.firebaseio.com/v0/topstories.json'
+
+  const fetchData = async () => 
+    {
+    const data = await fetch(dataUrl);
+    const dataJson = await data.json();
+
+    setStories(dataJson);
+    // console.log(dataJson);
+    }
+
   return (
     <div className="MainView">
       <h1>Main</h1>
       <Link to="/details">
         <span>Details View</span>
       </Link>
+      {stories.map(story => (
+        <h1 key={story} >{story}</h1>
+      ))}
     </div>
   );
 }
